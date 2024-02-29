b4f6993444eaf0e35fcc8743b5f6fa3f5b0cdc3c
CDharun <117619915+CDharun@users.noreply.github.com>
2024-02-29 16:09:58 +0530

    Add files via upload

---
+++ Backend/day3/mapper/planDataMapper.java
@@ -0,0 +1,38 @@
+package com.rechargenow.rechargenow.mapper;
+
+import com.rechargenow.rechargenow.Model.planData;
+import com.rechargenow.rechargenow.dto.planDataDTO;
+
+public class planDataMapper {
+    public static planDataDTO mapToPlanDataDTO(planData plan) {
+        planDataDTO planData = new planDataDTO();
+        // planData.setId(plan.getId());
+        planData.setOperator(plan.getOperator());
+        planData.setPlanType(plan.getPlanType());
+        planData.setPlanName(plan.getPlanName());
+        planData.setPlanValidity(plan.getPlanValidity());
+        planData.setPlanAmount(plan.getPlanAmount());
+        planData.setPlanDescription(plan.getPlanDescription());
+        planData.setPlanData(plan.getPlanData());
+        planData.setPlanTalktime(plan.getPlanTalktime());
+        planData.setPlanSms(plan.getPlanSms());
+        planData.setPlansubscription(plan.getPlansubscription());
+        return planData;
+    }
+
+    public static planData mapToPlanData(planDataDTO planData) {
+        planData plan = new planData();
+        plan.setOperator(planData.getOperator());
+        plan.setPlanType(planData.getPlanType());
+        plan.setPlanName(planData.getPlanName());
+        plan.setPlanValidity(planData.getPlanValidity());
+        plan.setPlanAmount(planData.getPlanAmount());
+        plan.setPlanDescription(planData.getPlanDescription());
+        plan.setPlanData(planData.getPlanData());
+        plan.setPlanTalktime(planData.getPlanTalktime());
+        plan.setPlanSms(planData.getPlanSms());
+        plan.setPlansubscription(planData.getPlansubscription());
+        return plan;
+    }
+    
+}
